007cec0ddbeb207f415918806949ced79a865ba5
John Papa <john@johnpapa.net>
2015-09-04 08:49:42 -0400

    unused heroes caching ... not needed since we dont leave the client ... yet

--- src/app/hero-service.ts
+++ src/app/hero-service.ts
@@ -1,16 +1,6 @@
 import {HEROES, Hero} from './heroes';
 
 export class HeroService {
-	private _heroes: Hero[];
-
-	get heroes() {
-		if (!this._heroes) {
-			this._heroes = [];
-			this.getAllHeroes()
-		}
-		return this._heroes;
-	}
-
 	getAllHeroes() {
 		return Promise.resolve(HEROES);
 	}
