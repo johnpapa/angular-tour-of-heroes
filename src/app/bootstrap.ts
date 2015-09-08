b93fe90a380e1783f93972c02c3ac7d690e313bd
John Papa <john@johnpapa.net>
2015-09-07 21:31:08 -0400

    routing code ... initial pass

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,4 +1,38 @@
 import {bootstrap} from 'angular2/angular2';
 import {HeroesComponent} from './heroes-component';
 
-bootstrap(HeroesComponent);
+import {bind, View, Component} from 'angular2/angular2';
+import {ROUTER_DIRECTIVES, ROUTER_BINDINGS} from 'angular2/router';
+import {RouteConfig, RouterOutlet, Router} from 'angular2/router';
+import {LocationStrategy, Location, HashLocationStrategy } from 'angular2/router';
+
+import { HeroDetailComponent } from './hero-detail-component';
+
+@Component({
+  selector: 'my-app'
+})
+@View({
+  template: `<router-outlet></router-outlet>`,
+  directives: [RouterOutlet]
+})
+@RouteConfig([
+  { path: '/', redirectTo: '/heroes' },
+  { path: '/heroes', as: 'heroes', component: HeroesComponent },
+  { path: '/detail', as: 'detail', component: HeroDetailComponent }
+  // { path: '/detail/:id',    as: 'orders',     component: HeroDetailComponent    }
+])
+export class AppComponent {
+  constructor(private router: Router, private location: Location) {
+    this.router = router;
+    this.location = location;
+
+    //Manual navigation for now
+    //let url = browserLocation.path();
+    //router.navigate(url);
+  }
+}
+
+bootstrap(AppComponent, [
+		ROUTER_DIRECTIVES, ROUTER_BINDINGS,
+	bind(LocationStrategy).toClass(HashLocationStrategy)
+]);
