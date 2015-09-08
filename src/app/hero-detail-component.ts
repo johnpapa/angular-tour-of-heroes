b22f90262572713171a93ef41b16f27274d2be47
John Papa <john@johnpapa.net>
2015-09-07 22:05:57 -0400

    route param - no worky yet

--- src/app/hero-detail-component.ts
+++ src/app/hero-detail-component.ts
@@ -1,5 +1,7 @@
 import {Component, FORM_DIRECTIVES, View} from 'angular2/angular2';
+// import {RouteParams} from 'angular2/router';
 import {Hero} from './hero';
+import {HeroService} from './hero-service';
 
 @Component({
   selector: 'my-hero-detail', properties: ['hero']
@@ -12,4 +14,12 @@ import {Hero} from './hero';
 })
 export class HeroDetailComponent {
   hero: Hero;
+  id: number;
+
+  constructor(private _heroService: HeroService) { //, routeParams: RouteParams) {
+    // this.id = +(routeParams.get('id'));
+    // this._heroService.getHero(this.id).then((hero) => {
+    //   this.hero = hero[0];
+    // });
+  }
 }
