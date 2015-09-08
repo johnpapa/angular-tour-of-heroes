ad89e516f66dd2473b440f7eb8bb36aea8127e82
John Papa <john@johnpapa.net>
2015-09-08 19:56:40 -0400

    removed hash strategy in favor of path strategy. its html5, so lets do that.

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,10 +1,9 @@
-import {bind, bootstrap} from 'angular2/angular2';
-import {ROUTER_BINDINGS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {bootstrap} from 'angular2/angular2';
+import {ROUTER_BINDINGS} from 'angular2/router';
 import {HeroService} from './hero-service';
 import {ShellComponent} from './shell-component';
 
 bootstrap(ShellComponent, [
   ROUTER_BINDINGS,
-  HeroService,
-	bind(LocationStrategy).toClass(HashLocationStrategy)
+  HeroService
 ]);
