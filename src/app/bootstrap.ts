9a15c0f6c97ee3da4fd3add50ab9dbd192257404
John Papa <john@johnpapa.net>
2015-09-08 01:04:24 -0400

    removing code

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,44 +1,10 @@
-import {bootstrap} from 'angular2/angular2';
-import {HeroesComponent} from './heroes-component';
-import { HeroService } from './hero-service';
-import {ROUTES} from './config';
+import {bind, bootstrap} from 'angular2/angular2';
+import {ROUTER_BINDINGS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {HeroService} from './hero-service';
+import {ShellComponent} from './shell-component';
 
-import {bind, View, Component} from 'angular2/angular2';
-
-// ROUTER_DIRECTIVES = RouterOutlet and RouterLink
-/* ROUTER-BINDINGS = CONST_EXPR([RouteRegistry, Pipeline, CONST_EXPR(new Binding(LocationStrategy, {toClass: PathLocationStrategy})), Location, CONST_EXPR(new Binding(Router, {
-        toFactory: routerFactory,
-        deps: CONST_EXPR([RouteRegistry, Pipeline, Location, APP_COMPONENT])
-      }))]);
-*/
-import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_BINDINGS} from 'angular2/router'; // Router
-import {LocationStrategy, HashLocationStrategy, PathLocationStrategy } from 'angular2/router'; // Location
-
-import {HeroDetailComponent } from './hero-detail-component';
-import { AboutComponent } from './about-component';
-
-@Component({
-  selector: 'my-app'
-})
-@View({
-    template: `
-    <a [router-link]="['${ROUTES.heroes}']">Heroes</a>
-    <a [router-link]="['${ROUTES.about}']">About</a>
-    <router-outlet></router-outlet>
-  `,
-  directives: [ROUTER_DIRECTIVES]
-})
-@RouteConfig([
-  {path: '/', redirectTo: ROUTES.heroes},
-  {path: ROUTES.about, as: 'about', component: AboutComponent},
-  {path: ROUTES.heroes, as: 'heroes', component: HeroesComponent},
-  {path: ROUTES.detail + '/:id', as: 'detail', component: HeroDetailComponent}
-])
-export class AppComponent { }
-
-bootstrap(AppComponent, [
+bootstrap(ShellComponent, [
   ROUTER_BINDINGS,
-  HeroService, // gives this to everyone
+  HeroService,
 	bind(LocationStrategy).toClass(HashLocationStrategy)
-	// bind(LocationStrategy).toClass(PathLocationStrategy)
 ]);
