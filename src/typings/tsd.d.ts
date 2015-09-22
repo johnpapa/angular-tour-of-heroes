889c954c8138081f2061cacf583b0ee86f633db1
John Papa <john@johnpapa.net>
2015-09-21 21:03:48 -0400

    fixed tsd

--- src/typings/tsd.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="../../node_modules/angular2/bundles/typings/angular2/angular2.d.ts" />
-/// <reference path="../../node_modules/angular2/bundles/typings/angular2/http.d.ts" />
-/// <reference path="../../node_modules/angular2/bundles/typings/angular2/router.d.ts" />
