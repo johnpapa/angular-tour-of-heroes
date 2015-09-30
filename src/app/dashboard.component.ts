1c635af3dc7b7134a1e92b8803fabbc5de209e50
John Papa <john@johnpapa.net>
2015-09-29 22:36:43 -0400

    removed img

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -2,9 +2,6 @@ import {Component, View} from 'angular2/angular2';
 
 @Component({ selector: 'my-dashboard' })
 @View({
-	template: `
-	<h2>Tour of Heroes Starts Here</h2>
-	<img src="./app/ng.png"/>
-	`
+	template: '<h2>Tour of Heroes Starts Here</h2>'
 })
 export class DashboardComponent { }
