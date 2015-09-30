ba93e613bc25c44d0417c648fcff75201a35a86f
John Papa <john@johnpapa.net>
2015-09-29 22:37:00 -0400

    trimming

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,7 +1,5 @@
 import {Component, View} from 'angular2/angular2';
 
 @Component({ selector: 'my-dashboard' })
-@View({
-	template: '<h2>Tour of Heroes Starts Here</h2>'
-})
+@View({template: '<h2>Tour of Heroes Starts Here</h2>'})
 export class DashboardComponent { }
