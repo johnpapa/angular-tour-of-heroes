36121e08fe8e284de2c7b0901e86e2b7a49f0c0c
John Papa <john.l.papa@disney.com>
2015-10-05 14:41:59 -0400

    added route class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import {View, Component, CORE_DIRECTIVES} from 'angular2/angular2';
-import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {RouteConfig, Router, ROUTER_DIRECTIVES} from 'angular2/router';
 import {Routes, APP_ROUTES} from './route.config';
 
 @Component({ selector: 'my-app' })
 @View({
   template: `
     <h1>{{title}}</h1>
-    <a [router-link]="['/${Routes.dashboard.as}']">Dashboard</a>
-    <a [router-link]="['/${Routes.heroes.as}']">Heroes</a>
+    <a [router-link]="['/${Routes.dashboard.as}']" [ng-class]="isRouteActive('/${Routes.dashboard.as}')">Dashboard</a>
+    <a [router-link]="['/${Routes.heroes.as}']" [ng-class]="isRouteActive('/${Routes.heroes.as}')">Heroes</a>
     <router-outlet></router-outlet>
   `,
   directives: [CORE_DIRECTIVES, ROUTER_DIRECTIVES]
@@ -15,4 +15,10 @@ import {Routes, APP_ROUTES} from './route.config';
 @RouteConfig(APP_ROUTES)
 export class AppComponent {
   public title = 'Tour of Heroes';
+
+  constructor(private _router: Router) {  }
+
+  isRouteActive(tuple: string) {
+    return { 'active': this._router.isRouteActive(this._router.generate([tuple]))};
+  }
 }
