39a603c85de859e2803095bffbe537d5612eb9f6
John Papa <john@johnpapa.net>
2015-10-13 22:27:31 -0400

    dont show the hero til its here

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import {Component, FORM_DIRECTIVES, OnInit} from 'angular2/angular2';
+import {Component, CORE_DIRECTIVES, FORM_DIRECTIVES, OnInit} from 'angular2/angular2';
 import {RouteParams, Router} from 'angular2/router';
 import {Hero} from './hero';
 import {HeroService} from './hero.service';
@@ -8,7 +8,7 @@ import {Routes} from './route.config';
 @Component({
   selector: 'my-hero-detail',
   templateUrl: 'app/hero-detail.component.html',
-  directives: [FORM_DIRECTIVES],
+  directives: [CORE_DIRECTIVES, FORM_DIRECTIVES],
   inputs: ['hero']
 })
 export class HeroDetailComponent implements OnInit {
