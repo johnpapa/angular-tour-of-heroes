346cf248237ebba284a0772977f9df322f3762f6
John Papa <john@johnpapa.net>
2015-10-13 22:27:21 -0400

    ROUTER BINDINGS are back

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,6 +1,6 @@
 import {bootstrap} from 'angular2/angular2';
-import {routerBindings} from 'angular2/router';
+import {ROUTER_BINDINGS} from 'angular2/router';
 import {HeroService} from './hero.service';
 import {AppComponent} from './app.component';
 
-bootstrap(AppComponent, [routerBindings(AppComponent), HeroService]);
+bootstrap(AppComponent, [ROUTER_BINDINGS, HeroService]);
