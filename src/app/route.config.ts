46b02fbd660674e9ce6246e5f32d0eee7de79d98
John Papa <john@johnpapa.net>
2015-11-07 20:14:18 -0500

    route revisions. now using the angualr 2 route class

--- src/app/route.config.ts
+++ src/app/route.config.ts
@@ -1,19 +1,12 @@
 import {HeroesComponent} from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
 import {DashboardComponent} from './dashboard.component';
-import {Router} from 'angular2/router';
-
-export class Route {
-  constructor(
-    public path:string,
-    public as:string,
-    public component:any ) { }
-}
+import {Route, Router} from 'angular2/router';
 
 export var Routes = {
-  dashboard: new Route('/', 'Dashboard', DashboardComponent ),
-  heroes: new Route('/heroes', 'Heroes', HeroesComponent ),
-  detail: new Route('/detail/:id', 'Detail', HeroDetailComponent )
+  dashboard: new Route({path: '/', as: 'Dashboard', component: DashboardComponent}),
+  heroes: new Route({path: '/heroes', as: 'Heroes', component: HeroesComponent}),
+  detail: new Route({path: '/detail/:id', as: 'Detail', component: HeroDetailComponent})
 };
 
 export const APP_ROUTES = Object.keys(Routes).map(r => Routes[r]);
