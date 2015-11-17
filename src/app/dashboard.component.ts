a536b6fbf624850d1dca4435d14b4f1fbe5b53e8
John Papa <john@johnpapa.net>
2015-11-17 00:25:11 -0500

    merged getHeroes into onInit

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -6,29 +6,20 @@ import {ROUTE_NAMES} from './route.config';
 
 @Component({
   selector: 'my-dashboard',
-	templateUrl: 'app/dashboard.component.html',
-	styleUrls: ['app/dashboard.component.css'],
+  templateUrl: 'app/dashboard.component.html',
+  styleUrls: ['app/dashboard.component.css'],
   directives: [CORE_DIRECTIVES]
 })
 export class DashboardComponent implements OnInit {
-	public heroes: Hero[];
+  public heroes: Hero[];
 
   constructor(private _heroService: HeroService, private _router: Router) { }
 
-	onInit() {
-    this.heroes = this.getHeroes();
+  onInit() {
+    this._heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
   gotoDetail(hero: Hero) {
     this._router.navigate([ROUTE_NAMES.heroDetail, { id: hero.id }]);
-  }
-
-  getHeroes() {
-    this.heroes = [];
-
-    this._heroService.getHeroes()
-      .then((heroes: Hero[]) => this.heroes = heroes);
-
-    return this.heroes;
   }
 }
