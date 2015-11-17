f03f7ee59bb4ad1dbc059f915183a5da7ec3cf20
John Papa <john@johnpapa.net>
2015-11-17 00:19:24 -0500

    moved to hash strategy. html5 mode (path) is not working right for me right now

--- src/app/boot.ts
+++ src/app/boot.ts
@@ -1,10 +1,11 @@
-import {bootstrap} from 'angular2/angular2';
-import {ROUTER_PROVIDERS} from 'angular2/router';
+import {bootstrap, provide} from 'angular2/angular2';
+import {HashLocationStrategy, LocationStrategy, ROUTER_PROVIDERS} from 'angular2/router';
 import {HeroService} from './hero.service';
 import {AppComponent} from './app.component';
 
 bootstrap(AppComponent, [
   ROUTER_PROVIDERS,
-  HeroService
+  HeroService,
+  provide(LocationStrategy, {useClass: HashLocationStrategy})
   // provide(APP_BASE_HREF, {useValue: '/app/'})
 ]);
