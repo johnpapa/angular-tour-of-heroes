dd71da7c87d2b4e747b0a9b436d2ee8f480befea
John Papa <john@johnpapa.net>
2015-11-17 00:12:52 -0500

    fixed routing classes to be easier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,15 @@ import {ROUTE_NAMES, ROUTES} from './route.config';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <a [router-link]="[routes.dashboard]" class="router-link">Dashboard</a>
-    <a [router-link]="[routes.heroes]" class="router-link">Heroes</a>
+    <a [router-link]="[routes.dashboard]">Dashboard</a>
+    <a [router-link]="[routes.heroes]">Heroes</a>
     <router-outlet></router-outlet>
   `,
   styles: [`
-    .router-link {padding: 5px;text-decoration: none;}
-    .router-link:visited, .router-link:link {color: #444;}
-    .router-link:hover {color: white; background-color: #1171a3; text-decoration: none;}
-    .router-link.router-link-active {color: white; background-color: #52b9e9; text-decoration: none;}
+    a {padding: 5px;text-decoration: none;}
+    a:visited, a:link {color: #444;}
+    a:hover {color: white; background-color: #1171a3; text-decoration: none;}
+    a.router-link-active {color: white; background-color: #52b9e9; text-decoration: none;}
   `],
   directives: [ROUTER_DIRECTIVES]
 })
