55ac6460eb5bb9b09aa41dd228760f4cfb088ebe
John Papa <john.l.papa@disney.com>
2015-12-02 09:53:31 -0500

    redirectto is now an any[]

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -10,7 +10,7 @@ export const ROUTE_NAMES = {
 }
 
 export const ROUTES: RouteDefinition[] = [
-  {path: '/', redirectTo: '/dashboard' },
+  {path: '/', redirectTo: ['/dashboard'] },
   {path: '/dashboard', name: ROUTE_NAMES.dashboard, component: DashboardComponent},
   {path: '/heroes', name: ROUTE_NAMES.heroes, component: HeroesComponent},
   {path: '/detail/:id', name: ROUTE_NAMES.heroDetail, component: HeroDetailComponent}
