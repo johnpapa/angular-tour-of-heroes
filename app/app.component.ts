84e81f6da94d6bc58280f840975787e241ec6b11
John Papa <john@johnpapa.net>
2015-12-23 00:49:33 -0500

    new css

--- app/app.component.ts
+++ app/app.component.ts
@@ -13,12 +13,7 @@ import {HeroService} from './hero.service';
     <a [routerLink]="['Heroes']">Heroes</a>
     <router-outlet></router-outlet>
   `,
-  styles: [`
-    a {padding: 5px;text-decoration: none;}
-    a:visited, a:link {color: #444;}
-    a:hover {color: white; background-color: #1171a3;}
-    a.router-link-active {color: white; background-color: #52b9e9;}
-  `],
+  styleUrls: ['app/app.component.css'],
   directives: [ROUTER_DIRECTIVES],
   providers: [HeroService]
 })
