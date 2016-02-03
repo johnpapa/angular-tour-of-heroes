658dc6ddb147df245c4246c92b83e18395b7c613
John Papa <john@johnpapa.net>
2016-02-03 15:06:11 -0500

    beta 3

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from 'angular2/core';
+import { Component, Input, OnInit } from 'angular2/core';
 import { RouteParams, ROUTER_DIRECTIVES } from 'angular2/router';
 import { Hero, HeroService } from './hero.service';
 
@@ -6,11 +6,10 @@ import { Hero, HeroService } from './hero.service';
   selector: 'my-hero-detail',
   templateUrl: 'app/hero-detail.component.html',
   styleUrls: ['app/hero-detail.component.css'],
-  directives: [ROUTER_DIRECTIVES],
-  inputs: ['hero']
+  directives: [ROUTER_DIRECTIVES]
 })
 export class HeroDetailComponent implements OnInit {
-  public hero: Hero;
+  @Input() hero: Hero;
 
   constructor(private _heroService: HeroService,
     private _routeParams: RouteParams) {
