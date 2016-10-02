eee80fa861865e67b3177e59157ad4e33f5cc9bb
John Papa <john@johnpapa.net>
2016-10-02 15:28:51 -0400

    sigh

--- system-config.js
+++ system-config.js
@@ -1,60 +1,33 @@
-/***********************************************************************************************
- * User Configuration.
- **********************************************************************************************/
-/** Map relative paths to URLs. */
-var map = {
-    'app': 'app',
-    'main': 'app/main.js',
-    '@angular': 'node_modules/@angular',
-    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    'rxjs': 'node_modules/rxjs'
-};
-// packages tells the System loader how to load when no filename and/or no
-// extension
-var packages = {
-    'app': { main: 'main.js', defaultExtension: 'js' },
-    'api': { defaultExtension: 'js' },
-    'rxjs': { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
-};
-var barrels = [
-    // App specific barrels.
-    'app/routing',
-    'app/shared',
-    'app/models',
-];
-barrels.forEach(function (barrelName) {
-    packages[barrelName] = { main: 'index' };
+System.config({
+    paths: {
+        // paths serve as alias
+        'npm:': 'node_modules/'
+    },
+    // map tells the System loader where to look for things
+    map: {
+        // our app is within the app folder
+        'app': 'app',
+        'main': 'app/main.js',
+        // angular bundles
+        '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+        '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+        '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+        '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+        '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+        '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+        '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+        '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+        '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
+        // other libraries
+        'rxjs': 'npm:rxjs',
+        'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+    },
+    // packages tells the System loader how to load when no filename and/or no extension
+    packages: {
+        'app': { main: './main.js', defaultExtension: 'js' },
+        'api': { defaultExtension: 'js' },
+        'rxjs': { defaultExtension: 'js' },
+        'angular-in-memory-web-api': { main: './index.js', defaultExtension: 'js' },
+    }
 });
-////////////////////////////////////////////////////////////////////////////////////////////////
-/***********************************************************************************************
- * Everything underneath this line is managed by the CLI.
- **********************************************************************************************/
-var ngPackageNames = [
-    'common',
-    'compiler',
-    'core',
-    'forms',
-    'http',
-    'platform-browser',
-    'platform-browser-dynamic',
-    'router',
-    'router-deprecated',
-    'upgrade',
-];
-// Individual files (~300 requests):
-function packIndex(pkgName) {
-    packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
-}
-// Bundled (~40 requests):
-function packUmd(pkgName) {
-    packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-}
-// Most environments should use UMD; some (Karma) need the individual index
-// files
-var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-// Add package entries for angular packages
-ngPackageNames.forEach(setPackageConfig);
-var config = { map: map, packages: packages };
-System.config(config);
 //# sourceMappingURL=system-config.js.map
