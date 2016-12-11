03335f21d305157b607f42d218b5cafaaf2b7eb4
John Papa <john@johnpapa.net>
2016-12-11 13:31:35 -0500

    rxjs

--- app/hero-search.service.ts
+++ app/hero-search.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
 import { Hero } from './hero';
 
 @Injectable()
@@ -11,6 +15,10 @@ export class HeroSearchService {
   search(term: string): Observable<Hero[]> {
     return this.http
       .get(`app/heroes/?name=${term}`)
-      .map((r: Response) => r.json().data as Hero[]);
+      .map((r: Response) => r.json().data as Hero[])
+      .catch((error: any) => {
+          console.error('An error occurred', error);
+          return Observable.throw(error.message || error);
+      });
   }
 }
